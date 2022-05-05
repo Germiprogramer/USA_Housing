a9f56c9be1cc738bc84d27797919dbc4834190f9
Germiprogramer <llorentemunozgerman@gmail.com>
2022-05-05 21:04:58 +0200

    importados datos y realizada primera parte del EDA

---
+++ analisis.py
@@ -0,0 +1,56 @@
+from classes.estadisticos import *
+
+casas = pd.read_csv("USA_Housing.csv")
+
+# ANALISIS DEL DATASET
+print("ANÁLISIS DEL DATASET")
+
+# Sustituimos el nombre de los valores a español
+casas.rename(columns = {'Avg. Area Income': 'Ingresos', 'Avg. Area House Age': 'Edad', 'Avg. Area Number of Rooms': 'Salas', 'Avg. Area Number of Bedrooms': 'Dormitorios', 'Area Population': 'Area', 'Price': 'Precio', 'Adress': 'Direccion'}, inplace = True)
+
+
+#Numero de filas
+filas = Filas(casas)
+filas.calculo()
+
+#Numero de columnas
+columnas = Columnas(casas)
+columnas.calculo()
+
+#Valores maximos
+max = Maximos(casas, "Precio")
+max.calculo()
+#Valores minimos
+min = Minimos(casas, "Precio")
+min.calculo()
+
+#Media
+media_HP = Media(casas, "Precio")
+media_HP.calculo()
+
+#Mediana
+mediana_HP = Mediana(casas, "Precio")
+mediana_HP.calculo()
+
+#Moda
+moda_HP = Moda(casas, "Precio")
+moda_HP.calculo()
+
+#Desviación típica
+desv_HP = Desviacion_tipica(casas, "Precio")
+desv_HP.calculo()
+
+#q1
+q1 = Q1(casas, "Precio")
+q1.calculo()
+
+
+#q3
+q3 = Q3(casas, "Precio")
+q3.calculo()
+
+#rango intercuartilico
+a = casas["Precio"].quantile(0.25)
+b = casas["Precio"].quantile(0.75)
+rango_int = b - a
+print("El rango intercuartilico es {}".format(rango_int))
---
+++ classes/estadisticos.py
@@ -0,0 +1,115 @@
+import pandas as pd
+
+class Media:
+    def __init__(self, csv, columna):
+        self.csv = csv
+        self.columna = columna
+
+    def calculo(self):
+        media = self.csv[self.columna].mean()
+        print("La media es:")
+        print(media)
+
+class Desviacion_tipica:
+    def __init__(self, csv, columna):
+        self.csv = csv
+        self.columna = columna
+
+    def calculo(self):
+        desv = self.csv[self.columna].mad()
+        print("La desviación tipica es:")
+        print(desv)
+
+class Varianza:
+    def __init__(self, csv, columna):
+        self.csv = csv
+        self.columna = columna
+
+    def calculo(self):
+        var = self.csv[self.columna].var()
+        print("La varianza es:")
+        print(var)
+
+class Filas:
+  def __init__(self, csv):
+    self.csv = csv
+
+  def calculo(self):
+    print('El numero de filas es:',self.csv.shape[0])
+
+class Columnas:
+  def __init__(self, csv):
+    self.csv = csv
+
+  def calculo(self):
+    print('El numero de columnas es:',self.csv.shape[1])
+
+class Maximos:
+  def __init__(self, csv, columna):
+      self.csv = csv
+      self.columna = columna
+
+  def calculo(self):
+      max = self.csv[self.columna].max()
+      print("El valor máximo es:")
+      print(max)
+
+class Minimos:
+  def __init__(self, csv, columna):
+      self.csv = csv
+      self.columna = columna
+
+  def calculo(self):
+      min = self.csv[self.columna].min()
+      print("El valor mínimo es:")
+      print(min)
+
+class Mediana:
+  def __init__(self, csv, columna):
+      self.csv = csv
+      self.columna = columna
+
+  def calculo(self):
+      median = self.csv[self.columna].median()
+      print("La mediana es:")
+      print(median)
+
+class Moda:
+  def __init__(self, csv, columna):
+      self.csv = csv
+      self.columna = columna
+
+  def calculo(self):
+      moda = self.csv[self.columna].mode()
+      print("La moda es:")
+      print(moda)
+
+class Rango:
+  def __init__(self, csv, columna):
+      self.csv = csv
+      self.columna = columna
+
+  def calculo(self):
+      rango = self.csv[self.columna].range()
+      print("La moda es:")
+      print(rango)
+
+class Q1:
+  def __init__(self, csv, columna):
+      self.csv = csv
+      self.columna = columna
+
+  def calculo(self):
+      q1 = self.csv[self.columna].quantile(0.25)
+      print("El primer cuartil es:")
+      print(q1)
+
+class Q3:
+  def __init__(self, csv, columna):
+      self.csv = csv
+      self.columna = columna
+
+  def calculo(self):
+      q3 = self.csv[self.columna].quantile(0.75)
+      print("El tercer cuartil es:")
+      print(q3)
