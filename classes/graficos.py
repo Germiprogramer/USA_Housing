1ae25376c911ba9614aed1cfdc93f1990f9207fd
Germiprogramer <llorentemunozgerman@gmail.com>
2022-05-06 19:31:30 +0200

    modificiacion ligera de la creargrafico

--- classes/graficos.py
+++ classes/graficos.py
@@ -3,9 +3,10 @@ import matplotlib.pyplot as plt
 def grafico(dataset, tipo_grafico, nombregrafico):
   fig, ax = plt.subplots()
  
-  dataset.plot(kind=tipo_grafico, ax = ax)
-  ax.set_title(str(nombregrafico), loc = "center", fontdict = {'fontsize':7, 'fontweight':'bold', 'color':'tab:blue'})
+  dataset.plot(kind=tipo_grafico, ax = ax, color = "red")
+  ax.set_title(str(nombregrafico), loc = "center", fontdict = {'fontsize':14, 'fontweight':'bold', 'color':'tab:orange'})
   ax.set_ylabel('')
+ 
   plt.show()
 
   plt.savefig('grafico.png', bbox_inches='tight')
