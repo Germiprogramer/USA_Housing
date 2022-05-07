7599cfa496d3efef0ce6d31ecbb0a862c5441649
Germiprogramer <llorentemunozgerman@gmail.com>
2022-05-07 12:22:49 +0200

    insertamos graficos de dispersion

--- classes/graficos.py
+++ classes/graficos.py
@@ -1,12 +1,25 @@
 import matplotlib.pyplot as plt
+import pandas as pd
 
-def grafico(dataset, tipo_grafico, nombregrafico):
+def grafico(dataset, tipo_grafico, titulografico, nombregrafico):
   fig, ax = plt.subplots()
  
   dataset.plot(kind=tipo_grafico, ax = ax, color = "red")
-  ax.set_title(str(nombregrafico), loc = "center", fontdict = {'fontsize':14, 'fontweight':'bold', 'color':'tab:orange'})
+  ax.set_title(str(titulografico), loc = "center", fontdict = {'fontsize':14, 'fontweight':'bold', 'color':'tab:orange'})
   ax.set_ylabel('')
  
   plt.show()
 
-  plt.savefig('grafico.png', bbox_inches='tight')
+  plt.savefig('{}.png'.format(nombregrafico), bbox_inches='tight')
+
+def dispersión(df, columna, nombregrafico):
+    
+    data=df[columna].groupby(pd.cut(df[columna], range(100,240,10))).count()
+    fig, ax = plt.subplots()
+    lista = []
+    for i in range(100,230,10):
+        lista.append(i)
+    plt.scatter(lista,data)
+    plt.title('Diagrama de dispersión:', color= 'orange')         
+    plt.show()
+    fig.savefig('{}.png'.format(nombregrafico))
