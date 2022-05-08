29a2428004de13ce3e0a751992f185fe175a8c42
Germiprogramer <llorentemunozgerman@gmail.com>
2022-05-08 10:05:17 +0200

    clustering

--- clustering.py
+++ clustering.py
@@ -5,6 +5,8 @@ from classes.graficos import *
 import matplotlib.pyplot as plt
 import numpy as np
 
+print("Se van a realizar una agrupación de las casas poniendo un precio límite.")
+
 casas = pd.read_csv("USA_Housing.csv")
 
 casas.rename(columns = {'Avg. Area Income': 'Ingresos', 'Avg. Area House Age': 'Edad', 'Avg. Area Number of Rooms': 'Salas', 'Avg. Area Number of Bedrooms': 'Dormitorios', 'Area Population': 'Area', 'Price': 'Precio', 'Address': 'Direccion'}, inplace = True)
